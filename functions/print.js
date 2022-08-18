a9244b8ff84f5e6f8de2b02cc9abd441ea63a515
Kanav Gupta <kanavgupta@Kanavs-MacBook-Pro.local>
2022-08-18 17:20:33 +0530

    serverless testing

--- functions/print.js
+++ functions/print.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 exports.handler = async function (event, context) {
-  async function fetchData() {
-    const res = axios.get("https://randomuser.me/api");
-    console.log(res.data.results[0].name.first);
-  }
-};
-fetchData();
-return {
-  statusCode: 200,
-  body: JSON.stringify({ message: "yoooo" }),
+  const fetchData = () => {
+    axios.get("https://randomuser.me/api").then((res) => {
+      console.log(res.data.results[0].name.first);
+    });
+  };
+  fetchData();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "yoooo" }),
+  };
 };
