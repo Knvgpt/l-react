6dd1a6801a7ad455c6a8abbc7a88838d2f063528
Kanav Gupta <kanavgupta@Kanavs-MacBook-Pro.local>
2022-08-18 17:32:28 +0530

    trying to see if netlify will deploy this or nah

--- functions/print.js
+++ functions/print.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 exports.handler = async function (event, context) {
+  const dt = "hello world";
   const fetchData = () => {
     axios.get("https://randomuser.me/api").then((res) => {
       console.log(res.data.results[0].name.first);
+      dt = res.data.results[0].name.first;
     });
   };
   fetchData();
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `${res.data.results[0].name.first}` }),
+    body: JSON.stringify({ message: `${dt}` }),
   };
 };
